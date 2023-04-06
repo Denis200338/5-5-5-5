d8f64d4c134fe01870403de0f66d8a44533c9acd
lochapok <mr.kuliush@mail.ru>
2023-04-06 12:21:18 +0300

    Создаём массив arr со значениями [4, -2, 6, 1, 3, 1, 0].
    Создаём переменную first_pos, которая будет хранить первый положительный элемент. На данный момент её значение равно None.
    
    Запускаем цикл for, который перебирает все элементы массива arr по порядку. Если очередной элемент num больше нуля, то мы записываем его значение в переменную first_pos и выходим из цикла с помощью оператора break.
    
    Если после выполнения цикла значение переменной first_pos не изменилось (осталось None), это означает, что в массиве нет положительных элементов. В этом случае мы выводим сообщение "Нет положительных элементов в массиве".
    
    Если первый положительный элемент был найден, то мы создаём переменную count, которая будет хранить количество элементов, равных первому положительному элементу. На данный момент её значение равно 0.
    
    Запускаем цикл for, который снова перебирает все элементы массива arr по порядку. Если очередной элемент num равен first_pos, то мы увеличиваем значение переменной count на 1.
    
    После выполнения цикла мы выводим сообщение о количестве элементов, равных первому положительному элементу, с помощью функции print и форматирования строк f-строками.
    
    В итоге, если массив arr содержит положительный элемент, то код выведет количество элементов в массиве, равных первому положительному элементу. Если в массиве нет положительных элементов, то будет выведено сообщение об этом.

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ for num in arr:
     if num > 0:
         first_pos = num
         break
+
+if first_pos is None:
+    print("Нет положительных элементов в массиве")
+else:
+    count = 0
+    for num in arr:
+        if num == first_pos:
+            count += 1
+    print(f"Количество элементов, равных первому положительному элементу ({first_pos}): {count}")
